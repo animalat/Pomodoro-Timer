774a9ea060b02a9676e9776f0161213e1bf3abf3
nakshayan <nakshayanplays@outlook.com>
2024-06-18 10:58:41 -0400

    started project (June 16)

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 500,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.setMenu(null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
---
+++ preload.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('Preload script loaded');
+});
+
